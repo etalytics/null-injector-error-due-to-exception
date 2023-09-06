5c28dd7d163c48abe5cd8fd2c3070b2337710d53
Peter Tandler <peter.tandler@teambits.de>
2023-09-06 16:24:48 +0200

    added failing service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {MyFailingService} from "./my-failing.service";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'null-injector-error-due-to-exception';
+  constructor(
+    private readonly myFailingService: MyFailingService
+  ) {
+    console.log('######## myFailingService = ', myFailingService);
+  }
 }
